7ee3880309f43188dff9ab344b28624211dd5492
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2023-03-23 00:32:36 +0100

    Make Wrong game winnable

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,25 +1,42 @@
 defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
+  defp randomize_answer() do
+    :rand.uniform(10) |> to_string
+  end
+
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok, assign(socket, score: 0, message: "Make a guess:", answer: randomize_answer())}
   end
 
   def handle_event("guess", %{"number" => guess}, socket) do
-    message = "Your guess: #{guess}. Wrong. Guess again."
-    score = socket.assigns.score - 1
+    answer = socket.assigns.answer
+    result = case guess do
+      ^answer -> %{
+        message: "Your guess was correct!",
+        score: socket.assigns.score + 1,
+        answer: randomize_answer()
+      }
+      _ -> %{
+        message: "Your guess: #{guess}. Wrong. Guess again.",
+        score: socket.assigns.score - 1,
+        answer: answer
+      }
+    end
 
     {
       :noreply,
       assign(
         socket,
-        message: message,
-        score: score
+        message: result.message,
+        score: result.score,
+        answer: result.answer
       )
     }
   end
 
-  @spec render(any) :: Phoenix::LiveView.Rendered.t()
+
+
   def render(assigns) do
     ~H"""
       <h1>Your score: <%= @score %></h1>
