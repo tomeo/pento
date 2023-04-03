858e39e57d714000e548dc6ac5637b5e0c50e664
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2023-04-03 03:36:36 +0200

    Add test db

--- config/test.exs
+++ config/test.exs
@@ -13,6 +13,7 @@ config :pento, Pento.Repo,
   password: "postgres",
   hostname: "localhost",
   database: "pento_test#{System.get_env("MIX_TEST_PARTITION")}",
+  port: 5434,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
