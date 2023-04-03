0df131e344559568bbe6f16b0ca52e9feaf41452
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2023-04-04 01:14:43 +0200

    Move guess to authed route

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -25,7 +25,6 @@ defmodule PentoWeb.Router do
   scope "/", PentoWeb do
     pipe_through :browser
 
-    live "/guess", WrongLive
     live "/products", ProductLive.Index, :index
     live "/products/new", ProductLive.Index, :new
     live "/products/:id/edit", ProductLive.Index, :edit
@@ -80,6 +79,7 @@ defmodule PentoWeb.Router do
       on_mount: [{PentoWeb.UserAuth, :ensure_authenticated}] do
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+      live "/guess", WrongLive
     end
   end
 
