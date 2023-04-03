965da1c261bb84c3d43b314b3d7886250cdf73ba
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2023-04-03 03:46:31 +0200

    Fix tests for auth

--- test/pento/accounts_test.exs
+++ test/pento/accounts_test.exs
@@ -97,22 +97,24 @@ defmodule Pento.AccountsTest do
   describe "change_user_registration/2" do
     test "returns a changeset" do
       assert %Ecto.Changeset{} = changeset = Accounts.change_user_registration(%User{})
-      assert changeset.required == [:password, :email]
+      assert changeset.required == [:username, :password, :email]
     end
 
     test "allows fields to be set" do
       email = unique_user_email()
+      username = unique_username()
       password = valid_user_password()
 
       changeset =
         Accounts.change_user_registration(
           %User{},
-          valid_user_attributes(email: email, password: password)
+          valid_user_attributes(username: username, email: email, password: password)
         )
 
       assert changeset.valid?
       assert get_change(changeset, :email) == email
       assert get_change(changeset, :password) == password
+      assert get_change(changeset, :username) == username
       assert is_nil(get_change(changeset, :hashed_password))
     end
   end
